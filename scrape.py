70eaf2aa41c9211c3e2152b399785221323a5a8a
Kishore Chowdhury <kishore.chowdhuri@gmail.com>
2021-05-13 20:17:36 +0530

    updated code

--- scrape.py
+++ scrape.py
@@ -1,7 +1,13 @@
 import requests
 import datetime
 import time
+import threading
 import send_email
+
+PINCODES = [700061, 700063, 700034] # use comma to separate pincodes
+PAYMENT_MODE = 'Free' # available values: Free, Paid
+AGE_LIMIT = 18 # available values: 18, 45
+
 
 def fetch_data(params):
 
@@ -9,18 +15,18 @@ def fetch_data(params):
         'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
     }    
     response = requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin', params=params, headers=headers, timeout=5)
-    # print(resp.url)
     return response
 
 
 def scrape(start_date, end_date, pincode, PAYMENT_MODE, AGE_LIMIT):
-
+    
     params = {
         'pincode': pincode,
         'date': start_date
     }
+    time.sleep(1)
     source = fetch_data(params)
-
+    
     if source.status_code != 200:
         print('Error occurred')
     else:
@@ -34,21 +40,21 @@ def scrape(start_date, end_date, pincode, PAYMENT_MODE, AGE_LIMIT):
             if item['fee_type'] == PAYMENT_MODE:
                 email_data  = f"{item['name'].upper()}\n"
                 email_data += f"Address: {item['address']} \n{item['district_name']}: {item['pincode']}\n"
-                email_sent = False
+                email_data += f"Date: {date_range}"
+                
                 for data in item['sessions']:
-                    if email_sent:
-                        break
-                    if data['min_age_limit'] == AGE_LIMIT and data['available_capacity'] > 0 and email_sent == False:
-                        email_data += f"Date: {date_range}"
+                    if data['min_age_limit'] == AGE_LIMIT and data['available_capacity'] > 0:
                         send_email.send_email_to_user(email_data, date_range)
-                        email_sent = True
                         print('Email sent successfully.')
+                        break
+    return True
 
-
-def preprocess(pincodes, PAYMENT_MODE, AGE_LIMIT):
+if __name__ == '__main__':
+    start = time.perf_counter()
     
-    #fetch data for 4 weeks
-    for pincode in pincodes:
+    threads = []
+    
+    for pincode in PINCODES:
         start_day = 0
         for _ in range(4):
             if start_day == 0:
@@ -62,17 +68,14 @@ def preprocess(pincodes, PAYMENT_MODE, AGE_LIMIT):
             
             start_dt = "%02d-%02d-%d" % (dt.day, dt.month, dt.year)
             end_dt = "%02d-%02d-%d" % (end_dt.day,end_dt.month,end_dt.year)
-            scrape(start_dt, end_dt, pincode, PAYMENT_MODE, AGE_LIMIT)
-            time.sleep(2)
+            
+            t = threading.Thread(target=scrape, args=[start_dt, end_dt, pincode, PAYMENT_MODE, AGE_LIMIT])
+            t.start()
+            threads.append(t)
 
-    return True
+        for thread in threads:
+            thread.join()
+        
 
-
-if __name__ == '__main__':
-    pincodes = [700061, 700063, 700034] # use comma to separate pincodes
-    PAYMENT_MODE = 'Free' # available values: Free, Paid
-    AGE_LIMIT = 18 # available values: 18, 45
-    print('Execution has started.')
-    preprocess(pincodes, PAYMENT_MODE, AGE_LIMIT)
-    print('Execution has finished.')
-    
+    finish = time.perf_counter()
+    print(f'finished in {round(finish - start, 5)} second(s)')
